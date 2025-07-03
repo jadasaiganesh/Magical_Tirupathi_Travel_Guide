9ed419c492e759b3d14e4879d3ef6e6a1b877c2f
SAI GANESH J <155799164+jadasaiganesh@users.noreply.github.com>
2025-07-03 15:50:35 +0530

    Add files via upload

---
+++ Magical_Tirupathi_Travel_Guide/homepage.js
@@ -0,0 +1,332 @@
+
+function Page() {
+    // Display an alert
+    alert('Click on OK to see more about TIRUMALA TIRUPATHI DEVASTHANAM');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'tirumala_tirupathi_devasthanam.html';
+}
+function varaha_swamy() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Varaha Swamy Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'varaha_swamy.html';
+}
+function vengamamba() {
+    // Display an alert
+    alert('Click on OK to see more about Matrusri Tarigonda Vengamamba Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Matrusri_Tarigonda_Vengamamba.html';
+}
+function raghavendra() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Raghavendra Swamy Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Sri_Raghavendra_swamy_Temple.html';
+}
+function japalianjaneya() {
+    // Display an alert
+    alert('Click on OK to see more about Japali Anjaneya Swami Devalayam');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Japali_Anjaneya_Swami_Devalayam.html';
+}
+function Akashaganga() {
+    // Display an alert
+    alert('Click on OK to see more about Akashaganga');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Akashaganga.html';
+}
+function Papavinasam() {
+    // Display an alert
+    alert('Click on OK to see more about Papavinasam');
+    
+    // Redirect to a new HTML file+
+    window.location.href = 'Papavinasam.html';
+}
+function venugopalaswamy() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Venugopala Swamy Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'venugopalaswamy.html';
+}
+function LakshmiNarasimhaSwamy() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Lakshmi Narasimha Swamy Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Sri Lakshmi Narasimha Swamy Temple.html';
+}
+function SriVakulamathaTemple() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Vakulamatha Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Sri Vakulamatha Temple.html';
+}
+function LalitaPeetam() {
+    // Display an alert
+    alert('Click on OK to see more about Lalita Peetam');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Lalita Peetam.html';
+}
+function SrikalyanaVenkateshwaraSwamyTemple() {
+    // Display an alert
+    alert('Click on OK to see more about Sri kalyana Venkateshwara Swamy Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Sri kalyana Venkateshwara Swamy Temple.html';
+}
+function ISKCONTemple() {
+    // Display an alert
+    alert('Click on OK to see more about ISKCON Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'ISKCON Temple.html';
+}
+function GovindarajaSwamyTemple() {
+    // Display an alert
+    alert('Click on OK to see more about Govindaraja Swamy Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Govindaraja Swamy Temple.html';
+}
+function KapilatheerthamTemple() {
+    // Display an alert
+    alert('Click on OK to see more about Kapilatheertham Temple');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Kapilatheertham Temple.html';
+}
+function SriVenkateswaraGuestHouse(){
+    // Display an alert
+    alert('Click on OK to see more about Sri Venkateswara Guest House, Tirumala');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'SriVenkateswaraGuestHouse.html';
+}
+function AdiSeshuGuestHouse()
+ {
+    // Display an alert
+    alert('Click on OK to see more about Adi Seshu Guest House');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Adi Seshu Guest House.html';
+}
+function SannidanamGuestHouse() {
+    // Display an alert
+    alert('Click on OK to see more about Sannidanam Guest House');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Sannidanam Guest House.html';
+}
+function VarahaswamyGuestHouse() {
+    // Display an alert
+    alert('Click on OK to see more about Varahaswamy Guest House');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Varahaswamy Guest House.html';
+}
+function MinervaGrand() {
+    // Display an alert
+    alert('Click on OK to see more about Minerva Grand');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Minerva Grand.html';
+}
+function HotelPaiViceroy() {
+    // Display an alert
+    alert('Click on OK to see more about Hotel Pai Viceroy');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Hotel Pai Viceroy.html';
+}
+function BhimasDeluxeHotel() {
+    // Display an alert
+    alert('Click on OK to see more about Bhimas DeluxeHotel');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'BhimasDeluxeHotel.html';
+}
+function GoldenTulip() {
+    // Display an alert
+    alert('Click on OK to see more about Golden Tulip');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Golden Tulip.html';
+}
+function TajHotel() {
+    // Display an alert
+    alert('Click on OK to see more about Taj Hotel');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Taj Hotel.html';
+}
+function SaarangiFineDineRestaurant() {
+    // Display an alert
+    alert('Click on OK to see more about Saarangi Fine Dine Restaurant');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Saarangi Fine Dine Restaurant.html';
+}
+function HotelSreeBalajiBhavan() {
+    // Display an alert
+    alert('Click on OK to see more about Hotel Sree Balaji Bhavan');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Hotel Sree Balaji Bhavan.html';
+}
+function SapthagiriShayaRestaurant() {
+    // Display an alert
+    alert('Click on OK to see more about Sapthagiri Shaya Restaurant');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Sapthagiri Shaya Restaurant.html';
+}
+function RoyalSarangeeFamilyRestaurant() {
+    // Display an alert
+    alert('Click on OK to see more about Royal Sarangee Family Restaurant');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Royal Sarangee Family Restaurant.html';
+}
+function Southernspice() {
+    // Display an alert
+    alert('Click on OK to see more about Southernspice');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Southernspice.html';
+}
+function SpicyParadise() {
+    // Display an alert
+    alert('Click on OK to see more about Spicy Paradise');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Spicy Paradise.html';
+}
+function GufaRestaurant() {
+    // Display an alert
+    alert('Click on OK to see more about Gufa Restaurant');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Gufa Restaurant.html';
+}
+function RoboDinner() {
+    // Display an alert
+    alert('Click on OK to see more about Robo Dinner');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Robo Dinner.html';
+}
+function Ps4Restaurant() {
+    // Display an alert
+    alert('Click on OK to see more about Ps4 Restaurant');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Ps4 Restaurant.html';
+}
+function SriVenkateswaraMuseum() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Venkateswara Museum');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Sri Venkateswara Museum.html';
+}
+function AkasagangaWaterfalls() {
+    // Display an alert
+    alert('Click on OK to see more about Akasaganga Waterfalls');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Akasaganga Waterfalls.html';
+}
+function ChandragiriFort() {
+    // Display an alert
+    alert('Click on OK to see more about Chandragiri Fort');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Chandragiri-Fort.html';
+}
+function SriVenkateswaraZologicalPark() {
+    // Display an alert
+    alert('Click on OK to see more about Sri Venkateswara Zological Park');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Sri Venkateswara Zological Park.html';
+}
+function MamanduruForest() {
+    // Display an alert
+    alert('Click on OK to see more about Mamanduru Forest');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Mamanduru Forest.html';
+}
+function TalakonaWaterfalls() {
+    // Display an alert
+    alert('Click on OK to see more about Talakona Waterfalls');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Talakona Waterfalls.html';
+}
+function BlulandWaterpark() {
+    // Display an alert
+    alert('Click on OK to see more about Bluland Waterpark');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Bluland Waterpark.html';
+}
+function RegionalSciencecentre() {
+    // Display an alert
+    alert('Click on OK to see more about Regional Science centre');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Regional Science centre.html';
+}
+function BoomBiker() {
+    // Display an alert
+    alert('Click on OK to see more about Boom Biker');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Boom Biker.html';
+}
+function CallBike() {
+    // Display an alert
+    alert('Click on OK to see more about Call Bike');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Call Bike.html';
+}
+function VijayBikeRental() {
+    // Display an alert
+    alert('Click on OK to see more about Vijay Bike Rental');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Vijay Bike Rental.html';
+}
+function TezgoBikes() {
+    // Display an alert
+    alert('Click on OK to see more about Tezgo Bikes ');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'Tezgo Bikes.html';
+}
+function TirupathiBikeRental() {
+    // Display an alert
+    alert('Click on OK to see more about Tirupathi Bike Rental');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'TirupathiBikeRental.html';
+}
+function SreeBikeBikeRental() {
+    // Display an alert
+    alert('Click on OK to see more about Sree Bike Rental ');
+    
+    // Redirect to a new HTML file
+    window.location.href = 'SreeBikeBikeRental.html';
+}
+
